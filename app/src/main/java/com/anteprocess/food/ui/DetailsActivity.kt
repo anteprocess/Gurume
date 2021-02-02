32cf7f887444b86bc1ee8f0a1b40b6690c04812b
Anteprocess <anteprocess@gmail.com>
2021-02-02 23:36:21 +0900

    Created Details Activity basic styles

---
+++ app/src/main/java/com/anteprocess/food/ui/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.anteprocess.food.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anteprocess.food.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
