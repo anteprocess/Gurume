1f8c967273ebb7aeb38338e8f6ab340874323960
Anteprocess <anteprocess@gmail.com>
2021-02-04 00:27:45 +0900

    Set Safe args for Details Activity

--- app/src/main/java/com/anteprocess/food/ui/DetailsActivity.kt
+++ app/src/main/java/com/anteprocess/food/ui/DetailsActivity.kt
@@ -1,12 +1,19 @@
 package com.anteprocess.food.ui
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.anteprocess.food.R
+import kotlinx.android.synthetic.main.activity_details.*
 
 class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
+
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
